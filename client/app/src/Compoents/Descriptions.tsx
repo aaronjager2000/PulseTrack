403d6da923fa515c6dcc1ca829bcb96d8c22d466
RYAN JAMES <realcoder2023@gmail.com>
2025-12-01 14:55:17 -0500

    addd some devices for main page styles

--- client/app/src/Compoents/Descriptions.tsx
+++ client/app/src/Compoents/Descriptions.tsx
@@ -1,9 +1,9 @@
 function Description(props: any) {
   return (
-    <div className="text-center grid  p-2 justify-center ">
+    <div className="flex text-center px-9 justify-center">
       <p
         id="description"
-        className="w-142 text-sm py-7 px-2 relative top-[200px] text-blue-900 font-bold mt-2"
+        className="max-md:text-xs py-7 relative top-[200px]  text-blue-900 font-bold mt-2"
       >
         {props.description}
       </p>
--- client/app/src/Compoents/Header.tsx
+++ client/app/src/Compoents/Header.tsx
@@ -1,13 +1,17 @@
 import Link from "next/link";
 
 function Header() {
-  if (new Date().getHours() > 15) {
+  if (new Date().getHours() < 15) {
     return (
-      <header className="md:flex justify-around md:px-9 md:bg-black md:py-5 md:text-black">
+      <header
+        className="max-sm:bg-wnite max-sm:text-sm  max-sm:font-bold flex justify-around max-md:justify-around bg-white max-md:p-2 max-md:pr-[300]
+      max-md:py-5 max-md:bg-white max-md:text-black py-5"
+      >
         <div>
-          <Link className="font-bold " href="/">
-            <span className="md:text-white font-black">Pulse</span>
-            <span className="md:text-white"> | Track</span>
+          <Link href="/">
+            <span className="maX-md-text:blue-900 text-1xs">
+              Pulse<span className="md:text-white text-2xs">Track</span>
+            </span>
           </Link>
         </div>
 
@@ -15,40 +19,16 @@ function Header() {
           <ul>
             <li>
               <Link
-                className="md:px p-3 text-md md:text-white text-blue-900
+                className="md:bg-white md:px p-3 max-md:hidden text-blue-900
               font-black text-"
                 href="/"
               >
                 Home
               </Link>
-              <Link className="px-5 font-bold text-md" href="/my_account">
-                My Account
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-    );
-  } else {
-    return (
-      <header className="lg:flex md:flex md:justify-around md:px-9 md:bg-white md:py-5 md:text-black">
-        <div>
-          <Link className="font-bold " href="/">
-            <span className="text-blue-900 uppercase">Pulse</span> | Track
-          </Link>
-        </div>
-
-        <nav>
-          <ul>
-            <li>
               <Link
-                className="md:bg-white md:px p-3 text-md text-blue-900
-              font-black text-"
-                href="/"
+                className="px-5 font-bold  text-blue-700 max-md:text-blue-900 max-md:hidden"
+                href="/my_account"
               >
-                Home
-              </Link>
-              <Link className="px-5 font-bold text-md" href="/my_account">
                 My Account
               </Link>
             </li>
@@ -57,6 +37,35 @@ function Header() {
       </header>
     );
   }
-}
 
+  return (
+    <header className="max-sm:bg-wnite max-sm:text-sm  max-sm:font-bold flex md:flex md:justify justify-around md:bg-white md:py-5 md:text-black">
+      <div>
+        <Link href="/">
+          <h4 className="max-md:text-2sm md:text-white">Pulse | Track</h4>
+        </Link>
+      </div>
+
+      <nav>
+        <ul>
+          <li>
+            <Link
+              className="md:px p-3 text-md md:text-white text-blue-900
+              font-black"
+              href="/"
+            >
+              Home
+            </Link>
+            <Link
+              className="px-5 font-bold text-md text-md text-blue-900"
+              href="/my_account"
+            >
+              My Account
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
 export default Header;
