c27294bbe2b5ded32b068e386ccd6f3ba73a91a5
RYAN JAMES <realcoder2023@gmail.com>
2025-12-01 00:21:51 -0500

    added more styles for footer to repo

--- Client/app/src/Compoents/Footer.tsx
+++ Client/app/src/Compoents/Footer.tsx
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <footer className="bg-white p-3 text-black text-center font-bold relative top-[100vh]">
+    <footer className="p-3 text-black text-center font-bold relative top-[100vh]">
       <div>
         <p>&copy; 2025 PulseTrack</p>
       </div>
