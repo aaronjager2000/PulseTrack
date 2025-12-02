7f0e179464b3639a0fec199547f64377853153c0
Artur Yoltukhivskyi <1234sleepyofficial@gmail.com>
2025-12-01 20:31:49 -0800

    fix 5

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,11 +1,9 @@
-import { Fragment } from "react/jsx-runtime";
-import Header from "./src/Compoents/Header";
-import HomeText from "./src/Compoents/HomeText";
-import Description from "./src/Compoents/Descriptions";
-import PricesSection from "./src/Compoents/Prices";
-import RegText from "./src/Compoents/RegText";
-import Footer from "./src/Compoents/Footer";
-import BoxSection from "./src/Compoents/BoxSeftion";
+import { Fragment } from 'react/jsx-runtime';
+import Header from './src/Compoents/Header';
+import HomeText from './src/Compoents/HomeText';
+import Description from './src/Compoents/Descriptions';
+import Footer from './src/Compoents/Footer';
+import BoxSection from './src/Compoents/BoxSeftion';
 
 function Home() {
   return (
